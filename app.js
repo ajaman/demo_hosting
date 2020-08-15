bf4ddcaa89a0ee9fd0f2a531d78d712fe483962a
ajaman <r.doleyz1993@gmail.com>
2020-08-16 02:47:39 +0530

    added env port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
-
+const port = process.env.port || 8080
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(8080);
+}).listen(port);
